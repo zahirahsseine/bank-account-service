8327bf4fee25dad7a9fd35db1a2f591edc4b0c60
zahirahsseine <zahirahsseine@gmail.com>
2023-03-11 06:57:26 +0100

    Add DTOS for other routes

--- src/main/java/org/side/bankaccountservice/service/AccountService.java
+++ src/main/java/org/side/bankaccountservice/service/AccountService.java
@@ -2,8 +2,13 @@ package org.side.bankaccountservice.service;
 
 import org.side.bankaccountservice.dto.BankAccountRequestDTO;
 import org.side.bankaccountservice.dto.BankAccountResponseDTO;
-import org.side.bankaccountservice.entities.BankAccount;
+
+import java.util.List;
 
 public interface AccountService {
-    BankAccountResponseDTO AddAccount(BankAccountRequestDTO bankAcountDTOResponse);
+    BankAccountResponseDTO AddAccount(BankAccountRequestDTO bankAcountDTORequest);
+    List<BankAccountResponseDTO> GetAllAccount();
+    BankAccountResponseDTO GetAccount(String id);
+    BankAccountResponseDTO UpdateAccount(String id, BankAccountRequestDTO bankAcountDTORequest);
+    void DeleteAccount(String id);
 }
--- src/main/java/org/side/bankaccountservice/service/AccountServiceImpl.java
+++ src/main/java/org/side/bankaccountservice/service/AccountServiceImpl.java
@@ -7,7 +7,9 @@ import org.side.bankaccountservice.repositories.BankAccountRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -15,6 +17,11 @@ import java.util.UUID;
 public class AccountServiceImpl implements AccountService {
 
     private BankAccountRepository bankAccountRepository;
+
+    public AccountServiceImpl(BankAccountRepository bankAccountRepository)
+    {
+        this.bankAccountRepository = bankAccountRepository;
+    }
     @Override
     public BankAccountResponseDTO AddAccount(BankAccountRequestDTO bankAcountDTO) {
         BankAccount bankAccount = BankAccount.builder()
@@ -34,5 +41,61 @@ public class AccountServiceImpl implements AccountService {
                 .currency(saveBankAccount.getCurrency())
                 .build();
         return bankAccountResponseDTO;
+    }
+
+    @Override
+    public List<BankAccountResponseDTO> GetAllAccount() {
+        var bankAccounts = bankAccountRepository.findAll();
+
+        List<BankAccountResponseDTO> listBankResponseDTO = new ArrayList<>();
+        bankAccounts.forEach(bankAccount->
+            listBankResponseDTO.add( BankAccountResponseDTO.builder()
+                    .id(bankAccount.getId())
+                    .createAt(bankAccount.getCreateAt())
+                    .balance(bankAccount.getBalance())
+                    .type(bankAccount.getType())
+                    .currency(bankAccount.getCurrency())
+                    .build())
+        );
+
+        return listBankResponseDTO;
+    }
+
+    @Override
+    public BankAccountResponseDTO GetAccount(String id) {
+        BankAccount bankAccount =  bankAccountRepository.findById(id)
+                                                        .orElseThrow();
+        BankAccountResponseDTO bankAccountResponseDTO = BankAccountResponseDTO.builder()
+                .id(bankAccount.getId())
+                .createAt(bankAccount.getCreateAt())
+                .balance(bankAccount.getBalance())
+                .type(bankAccount.getType())
+                .currency(bankAccount.getCurrency())
+                .build();
+        return bankAccountResponseDTO;
+    }
+
+    @Override
+    public BankAccountResponseDTO UpdateAccount(String id, BankAccountRequestDTO bankAcountDTORequest) {
+        BankAccount bankAccount = bankAccountRepository.findById(id).orElseThrow();
+        if(bankAcountDTORequest.getBalance()!=null) bankAccount.setBalance(bankAcountDTORequest.getBalance());
+        if(bankAcountDTORequest.getType()!=null) bankAccount.setType(bankAcountDTORequest.getType());
+        if(bankAcountDTORequest.getCurrency()!=null) bankAccount.setCurrency(bankAcountDTORequest.getCurrency());
+
+        BankAccount saveBankAccount = bankAccountRepository.save(bankAccount);
+        BankAccountResponseDTO bankAccountResponseDTO = BankAccountResponseDTO.builder()
+                .id(saveBankAccount.getId())
+                .createAt(saveBankAccount.getCreateAt())
+                .balance(saveBankAccount.getBalance())
+                .type(saveBankAccount.getType())
+                .currency(saveBankAccount.getCurrency())
+                .build();
+
+        return bankAccountResponseDTO;
+    }
+
+    @Override
+    public void DeleteAccount(String id) {
+        bankAccountRepository.deleteById(id);
     }
 }
